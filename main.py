4595d477d628ac7fc9f077fa40b21d8b159e59df
w03226am <augustas.macijauskas@student.manchester,ac.uk>
2020-07-13 15:44:23 +0300

    Data loader

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-import Network from networks/network_1
+import mnist_loader
+from networks import Network
 
 
 def main():
-    net = Network([2, 3, 1])
-
+    training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+    net = Network([784, 30, 10])
+    net.SGD(
+        training_data=training_data,
+        epochs=30,
+        mini_batch_size=10,
+        eta=3.0,
+        test_data=test_data
+    )
 
 if __name__ == "__main__":
     main()
